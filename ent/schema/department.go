4bba694a43609a071e3f8c8f1baa8a2ba5e92e50
DANH PHI LONG <longgggwww@gmail.com>
2025-05-13 11:21:14 +0700

    feat: add Position entity and related functionality

--- ent/schema/department.go
+++ ent/schema/department.go
@@ -1,6 +1,14 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/contrib/entproto"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
 
 // Department holds the schema definition for the Department entity.
 type Department struct {
@@ -9,10 +17,29 @@ type Department struct {
 
 // Fields of the Department.
 func (Department) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New).
+			Annotations(entproto.Field(1)),
+		field.String("name").Annotations(entproto.Field(2)),
+		field.String("code").
+			Unique().Annotations(entproto.Field(3)),
+		field.UUID("branch_id", uuid.UUID{}).Annotations(entproto.Field(4)),
+		field.Time("created_at").
+			Default(time.Now).Annotations(entproto.Field(5)),
+		field.Time("updated_at").
+			Default(time.Now).Annotations(entproto.Field(6)),
+	}
 }
 
 // Edges of the Department.
 func (Department) Edges() []ent.Edge {
 	return nil
+}
+
+func (Department) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entproto.Message(),
+		entproto.Service(),
+	}
 }
--- ent/schema/position.go
+++ ent/schema/position.go
@@ -1,6 +1,14 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/contrib/entproto"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
 
 // Position holds the schema definition for the Position entity.
 type Position struct {
@@ -9,10 +17,25 @@ type Position struct {
 
 // Fields of the Position.
 func (Position) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).Default(uuid.New).Annotations(entproto.Field(1)),
+		field.String("name").NotEmpty().Annotations(entproto.Field(2)),
+		field.String("code").Unique().NotEmpty().Annotations(entproto.Field(3)),
+		field.UUID("department_id", uuid.New()).Annotations(entproto.Field(4)),
+		field.UUID("parent_id", uuid.New()).Nillable().Optional().Annotations(entproto.Field(5)),
+		field.Time("created_at").Default(time.Now).Immutable().Annotations(entproto.Field(6)),
+		field.Time("updated_at").Default(time.Now).UpdateDefault(time.Now).Annotations(entproto.Field(7)),
+	}
 }
 
 // Edges of the Position.
 func (Position) Edges() []ent.Edge {
 	return nil
+}
+
+func (Position) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entproto.Message(),
+		entproto.Service(),
+	}
 }
