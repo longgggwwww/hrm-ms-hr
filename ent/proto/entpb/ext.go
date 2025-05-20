b5a4e68e8ecb2724f72298fb1713a70a9fa556d9
DANH PHI LONG <longgggwww@gmail.com>
2025-05-20 09:17:42 +0700

    feature: add ExtService with GetEmployeeByUserId and DeleteEmployeeByUserId methods

---
+++ ent/proto/entpb/ext.go
@@ -0,0 +1,38 @@
+package entpb
+
+import (
+	context "context"
+
+	"github.com/longgggwwww/hrm-ms-hr/ent"
+	employee "github.com/longgggwwww/hrm-ms-hr/ent/employee"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
+)
+
+// ExtService implements ExtServiceServer.
+type ExtService struct {
+	client *ent.Client
+	UnimplementedExtServiceServer
+}
+
+func (s *ExtService) GetEmployeeByUserId(ctx context.Context, req *GetEmployeeByUserIdRequest) (*Employee, error) {
+	e, err := s.client.Employee.Query().Where(employee.UserID(req.UserId)).Only(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return toProtoEmployee(e)
+}
+
+func (s *ExtService) DeleteEmployeeByUserId(ctx context.Context, req *DeleteEmployeeByUserIdRequest) (*emptypb.Empty, error) {
+	_, err := s.client.Employee.Delete().Where(employee.UserID(req.UserId)).Exec(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &emptypb.Empty{}, nil
+}
+
+// NewExtService returns a new ExtService.
+func NewExtService(client *ent.Client) *ExtService {
+	return &ExtService{
+		client: client,
+	}
+}
